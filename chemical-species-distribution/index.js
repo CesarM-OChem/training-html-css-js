f589f63e72332392731cd371e92b637f3ecb149e
Cesar_Macedo <czar92981@gmail.com>
2024-05-15 12:41:28 -0300

    Calculates correctly the alphas

--- chemical-species-distribution/index.js
+++ chemical-species-distribution/index.js
@@ -1,14 +1,17 @@
-const composts = {
-    'acetic acid': {
-        pKa: 4.76,
-        a0: 'HAc',
-        a1: 'Ac-'
-    },
-    'formic acid': {
-        pKa: 3.75,
-        a0: 'HCO2H',
-        a1: 'HCO2-'
-    }
+let composts = []
+let compostList = document.getElementById('compost')
+const points = 141
+const colorList = ['blue', 'red', 'green', 'yellow', 'purple', 'black', 'pink', 'gray']
+
+async function getData(){
+    let request = await fetch("https://raw.githubusercontent.com/CesarM-OChem/training-html-css-js/main/chemical-species-distribution/data.json")
+    let response = await request.json()
+    response.forEach(element => {
+        composts.push(element)
+        let option = document.createElement('option')
+        option.innerHTML = `<option value="${element.compost}">${element.compost}</option>`
+        compostList.appendChild(option)
+    })
 }
 
 const selectCompost = document.getElementById('compost')
@@ -18,66 +21,81 @@ let chart = null
 
 selectCompost.addEventListener('change', () => {
     const selectedCompost = selectCompost.value
-    const pKa = composts[selectedCompost]['pKa']
+    var properties = composts.filter(obj => {
+        return obj.compost === selectedCompost
+    })
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     if(chart){
         chart.destroy()
     }
 
+    const pKa = properties[0].pKa
+    const n = properties[0].protonNumber
     // calculate the concentrations using pka
-    const graphData = calculateGraphData(selectedCompost, pKa)
+    const graphData = calculateGraphData(pKa, n)
 
     // create graph with Chart.js
     var chartOptions = {
         responsive: false
     }
 
+    var dataset = []
+    for(let i = 0; i <= n; i++){
+        dataObject = {
+            label: properties[0].species[i],
+            data: graphData.alphaList[i],
+            borderColor: colorList[i],
+            fill: false
+        }
+        dataset.push(dataObject)
+    }
+
     chart = new Chart(canvas, {
         type: 'line',
         data: {
             labels: graphData.pHList,
-            datasets: [
-                {
-                    label: composts[selectedCompost].a0,
-                    data: graphData.alpha0List,
-                    borderColor: 'blue',
-                    fill: false
-                },
-                //New species datasets
-                {
-                    label: composts[selectedCompost].a1,
-                    data: graphData.alpha1List,
-                    borderColor: 'red',
-                    fill: false
-                }
-            ]
+            datasets: dataset
         },
         options: chartOptions
     })
 })
 
-function calculateGraphData(compost, pKa){
-    pHList = []
-    alpha0List = []
-    alpha1List = []
-    pH = 0
+function calculateGraphData(pKa, n){
+    pHList = new Array(points)
+    alphaList = Array.from({length: (n + 1)}, () => new Array(points))
+    pH = -0.1
 
-    for (let i = 0; i < 140; i++){
-        pHList.push(pH.toFixed(1))
+    for (let i = 0; i < points; i++){
         pH += 0.1
+        pHList[i] = pH.toFixed(1)
 
-        alpha1 = (1 / (1 + Math.pow(10, pKa - pH))).toFixed(5)
+        // calculate alpha0
+        sum = 0
+        for(let index = 1; index <= n; index++){
+            sum += sumExpTen(index, pH, pKa)
+        }
+        alphaList[0][i] = (1 / (1 + sum))
 
-        alpha1List.push(alpha1)
-        alpha0List.push(1 - alpha1)
+        // Calculate the other alphas
+        for(let alpha = 1; alpha <= n; alpha++){
+            alphaList[alpha][i] = (alphaList[0][i] * sumExpTen(alpha, pH, pKa))
+        }
     }
 
     return {
         pHList,
-        alpha0List,
-        alpha1List
+        alphaList
     }
+}
+
+function sumExpTen(index, pH, pKa){
+    exp = index * pH
+    for(let i = 0; i < index; i++){
+        exp -= pKa[i]
+    }
+
+    return Math.pow(10, exp)
 }
 
 //₂₃₄₅₆₇₈₉⁻⁺
